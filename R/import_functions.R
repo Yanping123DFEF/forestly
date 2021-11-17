7f8f8c34dd31ff71d825510de1a228d486049d12
LittleBeannie <yujie.zhao@merck.com>
2021-11-17 14:02:52 -0500

    fix CMD check

--- R/import_functions.R
+++ R/import_functions.R
@@ -1,5 +1,4 @@
 #' @importFrom plotly plot_ly
 #' @importFrom dplyr count group_by summarise n left_join mutate ungroup select across starts_with filter rename distinct group_map vars
 #' @importFrom tidyr pivot_wider complete
-#' @importFrom crosstool crosstool 
 NULL
